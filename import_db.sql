fbcc472209ed8706bf565117fc319a175261a6d4
vincentpham123 <vincentpham32@gmail.com>
2023-04-26 13:50:10 -0700

    finished sql database

--- import_db.sql
+++ import_db.sql
@@ -10,37 +10,43 @@ CREATE TABLE users (
 DROP TABLE if EXISTS questions;
 
 CREATE TABLE questions (
-    id INTEGER PRIMARY KEY ,
+    id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
-    body TEXT NOT NULL
+    body TEXT NOT NULL,
+    user_id INTEGER NOT NULL,
     
-    foreign key (user_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 DROP TABLE if EXISTS question_follows;
 
 CREATE TABLE question_follows (
     id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
 
     foreign key (user_id) REFERENCES users(id),
-    foreign key (question_id) REFERENCES questions(id),
+    foreign key (question_id) REFERENCES questions(id)
 );
 DROP TABLE if EXISTS replies;
 
 CREATE TABLE replies (
     id INTEGER PRIMARY KEY ,
     reply TEXT NOT NULL,
-
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+    reply_id INTEGER,
     foreign key (user_id) REFERENCES users(id),
     foreign key (question_id) REFERENCES questions(id),
     foreign key (reply_id) REFERENCES replies(id)
 );
-DROP TABLE if EXISTS question_likes;
+-- DROP TABLE if EXISTS question_likes;
 
 CREATE TABLE question_likes (
     id INTEGER PRIMARY KEY,
     liked BOOLEAN ,
-
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
     foreign key (user_id) REFERENCES users(id),
     foreign key (question_id) REFERENCES questions(id)
 );
@@ -75,6 +81,6 @@ VALUES
 INSERT INTO
     question_likes(liked,user_id,question_id)
 VALUES
-    (true,2,2)
-    (true,1,1);
+    (TRUE,2,2),
+    (TRUE,1,1);
 
